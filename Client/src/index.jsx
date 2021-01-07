bb92e389cb25ab46e7f4e4fbacba42bdd4ae772b
gkp-dev <gkprofessionel@gmail.com>
2021-01-07 09:32:55 +0000

    update server api

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './index.css';
 import App from './App';
 import SinglePokemon from './components/SinglePokemon/SinglePokemon';
-import reportWebVitals from './reportWebVitals';
 import 'materialize-css/dist/css/materialize.min.css';
 
 ReactDOM.render(
@@ -19,7 +18,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- Server/routes/pokemon.js
+++ Server/routes/pokemon.js
@@ -2,14 +2,20 @@ const { Router } = require('express');
 const router = Router();
 const pokedex = require('../data/pokedex.json');
 
-router.get('/:id', async(req, res) => {
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
 
-    await res.json(pokedex[req.params.id]);
+    if (!pokedex[id]) {
+        return res.status(404).json('The pokemon, you searched was not found...');
+    }
+    res.json(pokedex[req.params.id]);
 
 })
 
 router.get('/', (req, res) => {
-
+    if (!pokedex) {
+        return res.status(404).json('The list of pokemon was not found...');
+    }
     res.json(pokedex);
 
 })
